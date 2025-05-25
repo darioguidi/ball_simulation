9161516f3653753adb8a00d2bb7e77a22d8f37ac
Dario Guidi <dario.guidi954@gmail.com>
2025-05-25 12:30:47 +0200

    Refactor Makefile and main.c for SDL2 integration and error handling

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
+# Nome del file eseguibile
+TARGET = ball
+
+# File sorgenti
+SRC = main.c
+
+# Comando del compilatore
 CC = gcc
 
+# Flag per ottenere le opzioni di SDL2
+CFLAGS = $(shell sdl2-config --cflags)
+LDFLAGS = $(shell sdl2-config --libs)
+
+# Regola principale
+all: $(TARGET)
+
+$(TARGET): $(SRC)
+	$(CC) -o $(TARGET) $(SRC) $(CFLAGS) $(LDFLAGS)
+
+# Pulizia dei file compilati
+clean:
+	rm -f $(TARGET)
--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
 
-
-
 int main(void)
 {
-
-    if(SDL_Init == NULL){
+    if(SDL_Init(SDL_INIT_VIDEO) != 0){
         printf("Errore nella inizializzazone di SDL\n");
         return 1;
     }
 
     SDL_Window *window = SDL_CreateWindow(
+        "BALL BALL ball bal...",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         SCREEN_WIDTH,
@@ -28,6 +26,16 @@ int main(void)
     }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL){
+        printf("Errore nella inizializzazone di SDL\n");
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,0);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(5000);
+
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
